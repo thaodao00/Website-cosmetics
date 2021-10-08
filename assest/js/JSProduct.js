42598690abd3366956647f056c7b9c7738a0110f
yangheenlw <19130093@st.hcmuaf.edu.vn>
2021-10-08 16:49:31 +0700

    Thêm nội dung cho từng tab

--- assest/js/JSProduct.js
+++ assest/js/JSProduct.js
@@ -1,17 +1,32 @@
-var close = document.querySelector('.close-wrapper');
+let close = document.querySelector('.close-wrapper');
 
-close.onclick = function(e) {
+close.onclick = function() {
     document.querySelector('.stay-in-touch').style.display = 'none';
 }
 
-var quantityWantToBuy = document.querySelector('.quantity-buttons--number').innerText;
+let quantityWantToBuy = document.querySelector('.quantity-buttons--number').innerText;
 
-document.querySelector('.quantity-buttons--minus').onclick = function(e) {
+document.querySelector('.quantity-buttons--minus').onclick = function() {
     quantityWantToBuy >=1 ? quantityWantToBuy -= 1 : quantityWantToBuy = 0;
     document.querySelector('.quantity-buttons--number').innerText = quantityWantToBuy;
 }
 
-document.querySelector('.quantity-buttons--plus').onclick = function(e) {
+document.querySelector('.quantity-buttons--plus').onclick = function() {
     quantityWantToBuy = Number(quantityWantToBuy) + 1;
     document.querySelector('.quantity-buttons--number').innerText = quantityWantToBuy;
 }
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const tabs = $$('.product-content__tabs-item')
+
+const tabActive = $('.product-content__tabs-item.tabs-active')
+
+tabs.forEach((tab, index) => {
+    tab.onclick = function() {
+        $('.product-content__tabs-item.tabs-active').classList.remove('tabs-active')
+
+        this.classList.add('tabs-active')
+    }
+});
