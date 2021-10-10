4919219428954e6e3d1f3654c850c24488798b91
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2021-10-10 12:43:40 +0700

    Revert "Modal login và register"
    This reverts commit 881bd28853651e10835e8f11479e4b07193c0045.

--- assest/js/showHide.js
+++ assest/js/showHide.js
@@ -1,47 +1,7 @@
+// search
 const $$ = document.querySelectorAll.bind(document)
 const $ = document.querySelector.bind(document)
-//Modal show hide account
-const modalAccount = $('.modal-account')
-const iconUser = $('.login-link')
-const authForms = $$('.auth-form')
-const registerForm = $('.register-form')
-const  loginForm = $('.login-form')
-const loginFormBtn = $('.login-from__btn')
-const registerFormBtn = $('.register-from__btn')
-const backBtn= $$('.auth-form__controls-back')
-const btnLoginMobile = $('.btn-login')
-const navMobile = $('.nav-mobile')
-const navBtnMobile = $('.nav-link')
-function showAccount(){
-    modalAccount.classList.add('modal-account__open')
-}
-function hideAccount(){
-    modalAccount.classList.remove('modal-account__open')
-}
-btnLoginMobile.addEventListener('click',function (){
-    showAccount()
-})
-iconUser.addEventListener('click', showAccount)
-modalAccount.addEventListener('click',hideAccount)
-for(const authForm of authForms){
-    authForm.addEventListener('click', function (e){
-        e.stopPropagation()
-    })
-}
-loginFormBtn.addEventListener('click', function (){
-    loginForm.style.display="block"
-    registerForm.style.display="none"
 
-})
-registerFormBtn.addEventListener('click', function (){
-    registerForm.style.display="block"
-    loginForm.style.display="none"
-
-})
-for(const btn of backBtn){
-    btn.addEventListener('click', hideAccount)
-}
-//modal Show hide search
 const iconSearch = $('.search-link')
 const modal =  $('.modal')
 const iconClose = $('.modal__icon-close')
