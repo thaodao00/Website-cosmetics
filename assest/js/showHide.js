964877a3dfbb609dc873c4dacb4ccc0adecf8772
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2021-10-15 22:46:01 +0700

    fix css presonnal

--- assest/js/showHide.js
+++ assest/js/showHide.js
@@ -1,7 +1,7 @@
-// search
 const $$ = document.querySelectorAll.bind(document)
 const $ = document.querySelector.bind(document)
 
+//modal show hide search
 const iconSearch = $('.search-link')
 const modal =  $('.modal')
 const iconClose = $('.modal__icon-close')
@@ -27,23 +27,47 @@ searchHistory.addEventListener('click',function(event){
     event.stopPropagation()
 })
 
-//Slider
-var slideIndex =0
-showSlides()
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length)
-        {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+//Modal show hide account
+const modalAccount = $('.modal-account')
+const iconUsers = $$('.login-link')
+const authForms = $$('.auth-form')
+const registerForm = $('.register-form')
+const  loginForm = $('.login-form')
+const loginFormBtn = $('.login-from__btn')
+const registerFormBtn = $('.register-from__btn')
+const backBtn= $$('.auth-form__controls-back')
+const navMobile = $('.nav-mobile')
+const navBtnMobile = $('.nav-link')
+function showAccount(){
+    modalAccount.classList.add('modal-account__open')
 }
+function hideAccount(){
+    modalAccount.classList.remove('modal-account__open')
+}
+for(const iconUser of iconUsers) {
+    iconUser.addEventListener('click', showAccount)
+}
+
+modalAccount.addEventListener('click',hideAccount)
+for(const authForm of authForms){
+    authForm.addEventListener('click', function (e){
+        e.stopPropagation()
+    })
+}
+loginFormBtn.addEventListener('click', function (){
+    loginForm.style.display="block"
+    registerForm.style.display="none"
+
+})
+registerFormBtn.addEventListener('click', function (){
+    registerForm.style.display="block"
+    loginForm.style.display="none"
+
+})
+for(const btn of backBtn){
+    btn.addEventListener('click', hideAccount)
+}
+
+
+
+
